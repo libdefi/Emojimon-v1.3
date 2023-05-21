e8888ee453f8ab8f6e8a7c717e601117d78d424e
libdefi <libcreatesweb3@gmail.com>
2023-05-21 18:07:57 +0900

    Set ConnectWallet

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -7,6 +7,65 @@ import { App } from "./App";
 import { setup } from "./mud/setup";
 import { MUDProvider } from "./MUDContext";
 import { ToastContainer } from "react-toastify";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { Chain } from 'wagmi/chains';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const latticeTestChain: Chain = {
+  id: 4242,
+  name: 'LatticeTest',
+  network: 'LatticeTest',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'LatticeTest',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://follower.testnet-chain.linfra.xyz'],
+    },
+    public: {
+      http: ['https://follower.testnet-chain.linfra.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    etherscan: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+  },
+  testnet: false,
+};
+
+const { publicClient, chains } = configureChains(
+  [latticeTestChain],
+  [
+    jsonRpcProvider({
+      rpc: chain => ({
+        http: `https://follower.testnet-chain.linfra.xyz`,
+      }),
+    }),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'AutonomousNFTs',
+  projectId: 'AutonomousNFTs',
+  chains
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
+
+
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
@@ -15,10 +74,20 @@ const root = ReactDOM.createRoot(rootElement);
 // TODO: figure out if we actually want this to be async or if we should render something else in the meantime
 setup().then((result) => {
   root.render(
+    
     <MUDProvider value={result}>
-      <App />
-      <ToastContainer position="bottom-right" draggable={false} theme="dark" />
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <div className="mt-auto">
+            <ConnectButton />
+          </div>
+          
+          <App />
+          <ToastContainer position="bottom-right" draggable={false} theme="dark" />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </MUDProvider>
+ 
   );
   mountDevTools();
 });
